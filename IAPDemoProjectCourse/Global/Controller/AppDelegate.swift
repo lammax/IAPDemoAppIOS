4a25fc65cb478d824804b2064a57d9772ef40679
Lammax <lammax@mail.ru>
2020-08-25 17:19:55 +0300

    Initial commit

---
+++ IAPDemoProjectCourse/Global/Controller/AppDelegate.swift
@@ -0,0 +1,22 @@
+//
+//  AppDelegate.swift
+//  IAPDemoProject
+//
+//  Created by Ivan Akulov on 26/10/2017.
+//  Copyright © 2017 Ivan Akulov. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+        
+        return true
+    }
+}
+
---
+++ IAPDemoProjectCourse/Global/Model/CellIdentifiers.swift
@@ -0,0 +1,14 @@
+//
+//  CellIdentifiers.swift
+//  IAPDemoProject
+//
+//  Created by Ivan Akulov on 26/10/2017.
+//  Copyright © 2017 Ivan Akulov. All rights reserved.
+//
+
+import Foundation
+
+enum CellIdentifiers {
+    static let mainCell = "mainCell"
+    static let purchaseCell = "purchaseCell"
+}
